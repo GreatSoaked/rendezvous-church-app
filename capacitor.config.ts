38c00bc79ad764773ab950200452fc26e652fd15
Joshua Elias <elias.r.joshua@gmail.com>
2025-08-10 00:34:53 +0000

    Saving my project to transfer

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rendezvouschurch.app',
+  appName: 'rendezvouschurch',
+  webDir: 'public'
+};
+
+export default config;
